49695bdf2eee236111a56ad14a952d4324c7560b
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-09-05 11:24:49 +0200

    2_problem: divide & conquer

--- 2_problem.py
+++ 2_problem.py
@@ -1,3 +1,11 @@
-def myreduce(func, inplist):
+def myreduce(f, inputs):
     """A parallelizable reduce function"""
-    # ***YOUR CODE HERE***
+
+    while(True):
+        n = len(inputs)
+        if n == 1:
+            return inputs[0]
+        else:
+            inputs = ([f(inputs[i], inputs[n//2 + i])  # divide & conquer
+                       for i in range(n//2)] +
+                       list(inputs[n // 2 * 2:]))      # take care of uneven list here
