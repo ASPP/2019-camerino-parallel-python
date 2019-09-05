92c18b880bf25fd1cc25409463b980608c343376
ASPP Student <student@localhost>
2019-09-05 12:45:46 +0200

    better than yours, Jenni

--- 2_problem.py
+++ 2_problem.py
@@ -8,11 +8,9 @@ def myreduce(func, inplist):
         for pair in list(zip(inplist[::2], inplist[1::2])):
             output.append(func(pair[0],pair[1]))
 
-        if inplist % 2 != 0: 
+        if len(inplist) % 2 != 0: 
             output.append(inplist[-1])
 
         inplist = output
 
     return output
-
-
