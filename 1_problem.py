c9ab3d515b441fa343287e664353375d7ea8602c
ASPP Student <student@localhost>
2019-09-05 09:57:23 +0200

    uploading our cool super unique unicorn rainbow solution

--- 1_problem.py
+++ 1_problem.py
@@ -3,6 +3,9 @@ from dask import delayed  # DON'T CHANGE (explained later)
 def func(i):
     """A dummy CPU-bound function."""
     print(f'Function {i} starting...')
+    fakeList = []
+    for i in range(10000):
+        fakeList.append(i)
     # ***INSERT CODE HERE*** (should take about 1 second to execute)
     print(f'Function {i} done')
     return i
