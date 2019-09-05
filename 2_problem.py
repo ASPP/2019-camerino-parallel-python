9d5e28f12c9a79cdc25420616054f41f2fb654e8
ASPP Student <student@localhost>
2019-09-05 12:08:26 +0200

    we are the champs

--- 2_problem.py
+++ 2_problem.py
@@ -1,3 +1,18 @@
 def myreduce(func, inplist):
     """A parallelizable reduce function"""
     # ***YOUR CODE HERE***
+    while len(inplist) > 1:
+        
+        output = []
+        
+        for pair in list(zip(inplist[::2], inplist[1::2])):
+            output.append(func(pair[0],pair[1]))
+
+        if inplist % 2 != 0: 
+            output.append(inplist[-1])
+
+        inplist = output
+
+    return output
+
+
