9df01e11e7ef45336d45b8f29c13fc677175e7f1
ASPP Student <student@localhost>
2019-09-05 09:56:16 +0200

    random number generator

--- 1_problem.py
+++ 1_problem.py
@@ -1,9 +1,11 @@
 from dask import delayed  # DON'T CHANGE (explained later)
+import numpy as np
 
 def func(i):
     """A dummy CPU-bound function."""
     print(f'Function {i} starting...')
     # ***INSERT CODE HERE*** (should take about 1 second to execute)
+    np.random.rand(int(1e8))
     print(f'Function {i} done')
     return i
 
