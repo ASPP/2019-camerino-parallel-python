49f0d851c597f90324dff05a7c2867233f13c881
ASPP Student <student@localhost>
2019-09-05 09:57:33 +0200

    our one sec function

--- 1_problem.py
+++ 1_problem.py
@@ -1,9 +1,18 @@
-from dask import delayed  # DON'T CHANGE (explained later)
+from time import time
 
 def func(i):
     """A dummy CPU-bound function."""
     print(f'Function {i} starting...')
+    start = time()
     # ***INSERT CODE HERE*** (should take about 1 second to execute)
+    random_array = np.random.rand(5001000)
+    aux = []
+    for k in random_array:
+        if k > 0.5:
+            aux.append(k)
+        else:
+            aux.append(0)
+    print(time()-start)
     print(f'Function {i} done')
     return i
 
