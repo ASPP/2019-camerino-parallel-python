fc1561e703a7608d3d11e2b23d545cb9e72a5a29
ASPP Student <student@localhost>
2019-09-05 10:00:36 +0200

    completed func that lasts approx 1s

--- 1_problem.py
+++ 1_problem.py
@@ -4,7 +4,12 @@ def func(i):
     """A dummy CPU-bound function."""
     print(f'Function {i} starting...')
     # ***INSERT CODE HERE*** (should take about 1 second to execute)
+    sum = 0
+    for j in range(5000000):
+        sum = sum+j
+    print(sum)
+
     print(f'Function {i} done')
     return i
 
-lazy = [delayed(func)(i) for i in range(4)]  # DON'T CHANGE (explained later)
+lazy = [delayed(func(i)) for i in range(4)]  # DON'T CHANGE (explained later)
