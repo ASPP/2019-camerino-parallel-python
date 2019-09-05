4a89ce4ab1daad8bde0961658d7f8c607e99c8d4
ASPP Student <student@localhost>
2019-09-05 10:06:55 +0200

    solution using numpy

--- 1_problem.py
+++ 1_problem.py
@@ -1,10 +1,13 @@
 from dask import delayed  # DON'T CHANGE (explained later)
+import numpy as np
 
 def func(i):
     """A dummy CPU-bound function."""
     print(f'Function {i} starting...')
-    # ***INSERT CODE HERE*** (should take about 1 second to execute)
+    A = np.random.rand(750,750)
+    np.sum(np.linalg.eigvals(A))
     print(f'Function {i} done')
+
     return i
 
 lazy = [delayed(func)(i) for i in range(4)]  # DON'T CHANGE (explained later)
